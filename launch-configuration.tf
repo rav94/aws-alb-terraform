7cfc8fe8418629e78604046148aa65e8f0e5639b
RavinduNF <fernandoravindu1994@gmail.com>
2019-07-19 09:53:12 +0530

    Fixing variable values

--- launch-configuration.tf
+++ launch-configuration.tf
@@ -1,9 +1,9 @@
 resource "aws_launch_configuration" "nginx" {
-  lifecycle { create_before_destroy = true }
-
   image_id             = "${var.COREOS_AMI}"
   instance_type        = "t2.micro"
   key_name             = "rnf-sec"
   security_groups      = ["${aws_security_group.nginx.id}"]
   user_data            = "${data.ignition_config.nginx.rendered}"
+
+  lifecycle { create_before_destroy = true }
 }
